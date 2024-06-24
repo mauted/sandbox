d6a9428fbc300f7d5e388eb280cdfaab664f81b6
MauricioTedeschi <mauricioatedeschi@gmail.com>
2024-06-24 15:46:41 +0200

    quick tweak

--- src/com/mauted/sandbox/world/World.java
+++ src/com/mauted/sandbox/world/World.java
@@ -13,18 +13,18 @@ public class World {
   
   private Player player;
   private LinkedList<Entity> entities;
-  // private Tree[] trees;
+  private Tree[] trees;
   private WorldMap worldMap;
 
   public World(WorldMap worldMap) {
     this.worldMap = worldMap;
     this.player = new Player(0, 0);
     this.entities = new LinkedList<Entity>();
-    // this.trees = new Tree[100];
+    this.trees = new Tree[100];
 
-    // for (int i = 0; i < trees.length; i++) {
-    //   trees[i] = new Tree((int) (Math.random() * worldMap.getWidth()) * Tile.DEFAULT_TILE_SIZE, (int) (Math.random() * worldMap.getHeight()) * Tile.DEFAULT_TILE_SIZE);
-    // }
+    for (int i = 0; i < trees.length; i++) {
+      trees[i] = new Tree((int) (Math.random() * worldMap.getWidth()) * Tile.DEFAULT_TILE_SIZE, (int) (Math.random() * worldMap.getHeight()) * Tile.DEFAULT_TILE_SIZE);
+    }
   }
 
   public void update() {
@@ -58,11 +58,11 @@ public class World {
       }
     }
 
-    // for (Tree tree : trees) {
-    //   int xPos = Math.round(tree.getX() - cameraX);
-    //   int yPos = Math.round(tree.getY() - cameraY);
-    //   gamePanel.renderSprite(tree.getSprite(), xPos, yPos);
-    // }
+    for (Tree tree : trees) {
+      int xPos = Math.round(tree.getX() - cameraX);
+      int yPos = Math.round(tree.getY() - cameraY);
+      gamePanel.renderSprite(tree.getSprite(), xPos, yPos);
+    }
     
     int xPos = Math.round(player.getX() - cameraX);
     int yPos = Math.round(player.getY() - cameraY);
